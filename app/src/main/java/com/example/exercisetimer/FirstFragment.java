884b9d0c7c105e5fea43b69ce3409dd1c6ada315
= <=>
2020-09-21 14:21:57 -0700

    Basic timer with start and stop

--- app/src/main/java/com/example/exercisetimer/FirstFragment.java
+++ app/src/main/java/com/example/exercisetimer/FirstFragment.java
@@ -1,9 +1,11 @@
 package com.example.exercisetimer;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -23,11 +25,71 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        final long[] startTime = {System.currentTimeMillis()};
+
+        final TextView clockDisplay = (TextView) view.findViewById(R.id.clock_display);
+        final Handler clockHandler = new Handler();
+        clockDisplay.setText(getString(R.string.clock_display,0,0,0));
+        class clockRunnable implements Runnable {
+            private Handler clockHandler;
+            private TextView clockDisplay;
+            private boolean running;
+            private long startTime;
+            private long elapsedTime;
+            private long elapsedSeconds;
+            private long elapsedMinutes;
+            private long elapsedHours;
+
+
+            public clockRunnable(Handler clockHandler, long startTime, TextView clockDisplay){
+                this.clockHandler = clockHandler;
+                //this.startTime = startTime;
+                this.clockDisplay = clockDisplay;
+                this.running = false;
+                //this.elapsedTime = System.currentTimeMillis() - this.startTime;
+                //this.elapsedSeconds = this.elapsedTime/1000;
+                //this.elapsedMinutes = this.elapsedSeconds/60;
+                //this.elapsedHours = this.elapsedMinutes/60;
+            }
+
+            public void setStartTime(long startTime) {
+                this.startTime = startTime;
+            }
+
+            public void setRunning(boolean running) {
+                this.running = running;
+            }
+
+            @Override
+            public void run() {
+                if (this.running) {
+                    this.clockHandler.postDelayed(this, 500);
+                    this.elapsedTime = System.currentTimeMillis() - this.startTime;
+                    this.elapsedSeconds = this.elapsedTime / 1000;
+                    this.elapsedMinutes = this.elapsedSeconds / 60;
+                    this.elapsedHours = this.elapsedMinutes / 60;
+                    this.clockDisplay.setText(getString(R.string.clock_display,elapsedHours,elapsedMinutes,elapsedSeconds));
+                }
+            }
+        }
+
+        final clockRunnable clockRunner = new clockRunnable(clockHandler, startTime[0], clockDisplay);
+
+
+        view.findViewById(R.id.button_start).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                startTime[0] = System.currentTimeMillis();
+                clockRunner.setStartTime(startTime[0]);
+                clockRunner.setRunning(true);
+                clockHandler.post(clockRunner);
+            }
+        });
+
+        view.findViewById(R.id.button_stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clockRunner.setRunning(false);
             }
         });
     }
