7db30755321ede255c329abad782893a6d6285ca
= <=>
2020-09-25 20:32:37 -0700

    update variable name

--- app/src/main/java/com/example/exercisetimer/Data/AppDatabase.java
+++ app/src/main/java/com/example/exercisetimer/Data/AppDatabase.java
@@ -13,18 +13,18 @@ import com.example.exercisetimer.R;
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract WorkoutDao workoutDao();
-    private static AppDatabase Instance;
+    private static AppDatabase INSTANCE;
 
     static AppDatabase getDatabase(final Context context) {
-        if (Instance == null) {
+        if (INSTANCE == null) {
             synchronized (AppDatabase.class) {
-                if (Instance == null) {
-                    Instance = Room.databaseBuilder(context.getApplicationContext(),
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             AppDatabase.class, context.getResources().getString(R.string.database_name)).build();
                 }
             }
         }
-        return Instance;
+        return INSTANCE;
     }
 }
 
